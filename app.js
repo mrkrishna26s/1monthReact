59ba27e982899d71f97203462f6c7c654bccb259
mrkrishna26s <murari2632000@gmail.com>
2025-12-21 08:21:36 +0530

    episode1 is completed

---
+++ app.js
@@ -0,0 +1,10 @@
+const header = React.createElement("div", {id:"heading"},
+    React.createElement("div", {id:"child"},
+    [React.createElement("h1",{},"I am child in react"),
+     React.createElement("h3",{},"I am child in react")   
+     ]));
+ 
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(header);
