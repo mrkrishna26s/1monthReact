69826a685312a0a2ea76d8c13f7d7bf52835bcdc
mrkrishna26s <murari2632000@gmail.com>
2026-01-04 12:52:43 +0530

    Mastered how i can replace createReactApp with parcel bundler

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const header = React.createElement("div", {id:"heading"},
-    React.createElement("div", {id:"child"},
+    [React.createElement("h1", {id:"parent"}, "This is parent"),
+        React.createElement("div", {id:"child"},
     [React.createElement("h1",{},"I am child in react"),
      React.createElement("h3",{},"I am child in react")   
-     ]));
+     ])]);
  
 
 
