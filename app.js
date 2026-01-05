b0c7fcd68310580306474aac6f146263556e1985
mrkrishna26s <murari2632000@gmail.com>
2026-01-05 08:33:19 +0530

    started understanding jsx

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const header = React.createElement("div", {id:"heading"},
     [React.createElement("h1",{},"I am child in react"),
      React.createElement("h3",{},"I am child in react")   
      ])]);
+
+const jsxHeading = <h1>Namaste React using JSX</h1>;
+console.log(jsxHeading);
  
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(header);
+root.render(jsxHeading);
