9a0abdf2637bc5b8e9278f8d97cdeba20e9fe8e1
mrkrishna26s <murari2632000@gmail.com>
2026-01-05 10:09:58 +0530

    understood  functional component , component compositioning and much more

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
+//react element
 const header = React.createElement("div", {id:"heading"},
     [React.createElement("h1", {id:"parent"}, "This is parent"),
         React.createElement("div", {id:"child"},
@@ -10,8 +11,25 @@ const header = React.createElement("div", {id:"heading"},
 
 const jsxHeading = <h1>Namaste React using JSX</h1>;
 console.log(jsxHeading);
+
+//Functional component
+
+const Title =() =>{
+    return <h1>ARNAVI</h1>
+}
+const HeadingComponent = () => {
+    return (
+    <div>
+        <Title />
+        <h1>Namaste React using Functional Component</h1>
+    </div>);
+}
+
+const HeadingComponent2 = () => <h1 className="heading">Namaste React using Functional Component</h1>;
+
  
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxHeading);
+root.render(<HeadingComponent />)
